ba23096b21bd6fe79fe5a59ad03e60cd706ee9d5
AyeshaSaif97 <ayesha.saif13@gmail.com>
2023-10-21 14:44:25 -0400

    latest

---
+++ src/test/java/steps/EmployeeSearchSteps.java
@@ -0,0 +1,63 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utils.CommonMethods;
+
+import java.time.Duration;
+
+public class EmployeeSearchSteps extends CommonMethods {
+
+    // public WebDriver driver;
+
+    @When("user clicks on PIM option")
+    public void user_clicks_on_pim_option() {
+        WebElement pimOption = driver.findElement(By.id("menu_pim_viewPimModule"));
+        //  pimOption.click();
+        click(pimOption);
+      //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @When("user clicks on Employee List Option")
+    public void user_clicks_on_employee_list_option() {
+        WebElement empListOption = driver.findElement(By.id("menu_pim_viewEmployeeList"));
+        click(empListOption);
+        // empListOption.click();
+
+    }
+
+    @When("user enters employee id")
+    public void user_enters_employee_id() {
+        WebElement empIdTextBox = driver.findElement(By.id("empsearch_id"));
+        // empIdTextBox.sendKeys("65230336");
+        sendText(empIdTextBox, "65230336");
+
+
+    }
+
+    @When("user clicks on search button")
+    public void user_clicks_on_search_button() {
+        WebElement searchButton = driver.findElement(By.id("searchBtn"));
+        //searchButton.click();
+        click(searchButton);
+
+    }
+
+    @Then("user should be able to see employee details")
+    public void user_should_be_able_to_see_employee_details() {
+        System.out.println("Test passed");
+
+    }
+
+    @When("user enters valid employee name")
+    public void user_enters_valid_employee_name() {
+        WebElement nameTextField = driver.findElement(By.id("empsearch_employee_name_empName"));
+        // nameTextField.sendKeys("mali");
+        sendText(nameTextField, "mali");
+    }
+
+
+}
